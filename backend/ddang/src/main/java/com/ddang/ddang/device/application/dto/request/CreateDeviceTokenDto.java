f0c2439cdc30ad66dbf4dffd2cbae120977d4c31
apptie <appti@naver.com>
2023-11-30 21:21:58 +0900

    refactor: 인가 관련 요청 시 검증이 필요하지 않은 항목에 대해 @Nullable 추가

--- backend/ddang/src/main/java/com/ddang/ddang/device/application/dto/request/CreateDeviceTokenDto.java
+++ backend/ddang/src/main/java/com/ddang/ddang/device/application/dto/request/CreateDeviceTokenDto.java
@@ -1,8 +1,9 @@
 package com.ddang.ddang.device.application.dto.request;
 
 import com.ddang.ddang.device.presentation.dto.request.UpdateDeviceTokenRequest;
+import javax.annotation.Nullable;
 
-public record CreateDeviceTokenDto(String deviceToken) {
+public record CreateDeviceTokenDto(@Nullable String deviceToken) {
 
     public static CreateDeviceTokenDto from(final UpdateDeviceTokenRequest updateDeviceTokenRequest) {
         return new CreateDeviceTokenDto(updateDeviceTokenRequest.deviceToken());
