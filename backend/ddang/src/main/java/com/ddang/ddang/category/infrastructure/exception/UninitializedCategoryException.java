12b7314c98f0c0cbead8c643077d6fd2bb3ece74
apptie <appti@naver.com>
2023-12-02 19:06:57 +0900

    feat: 카테고리가 초기화되지 않았을 때 발생하는 예외 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/category/infrastructure/exception/UninitializedCategoryException.java
@@ -0,0 +1,8 @@
+package com.ddang.ddang.category.infrastructure.exception;
+
+public class UninitializedCategoryException extends RuntimeException {
+
+    public UninitializedCategoryException(final String message) {
+        super(message);
+    }
+}
