e1607be0d20d25c80baae7e67ac7f007abc2d332
apptie <appti@naver.com>
2023-12-02 19:11:20 +0900

    feat: 세 번째 지역이 유효하지 않은 경우에 발생하는 예외 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/application/exception/InvalidThirdRegionException.java
@@ -0,0 +1,8 @@
+package com.ddang.ddang.auction.application.exception;
+
+public class InvalidThirdRegionException extends IllegalArgumentException {
+
+    public InvalidThirdRegionException(final String message) {
+        super(message);
+    }
+}
--- backend/ddang/src/main/java/com/ddang/ddang/category/application/CategoryService.java
+++ backend/ddang/src/main/java/com/ddang/ddang/category/application/CategoryService.java
@@ -1,9 +1,9 @@
 package com.ddang.ddang.category.application;
 
 import com.ddang.ddang.category.application.dto.response.ReadCategoryDto;
-import com.ddang.ddang.category.infrastructure.exception.CategoryNotFoundException;
 import com.ddang.ddang.category.domain.Category;
 import com.ddang.ddang.category.domain.repository.CategoryRepository;
+import com.ddang.ddang.category.infrastructure.exception.UninitializedCategoryException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,7 +20,7 @@ public class CategoryService {
         final List<Category> mainCategories = categoryRepository.findMainAllByMainCategoryIsNull();
 
         if (mainCategories.isEmpty()) {
-            throw new CategoryNotFoundException("등록된 메인 카테고리가 없습니다.");
+            throw new UninitializedCategoryException("등록된 메인 카테고리가 없습니다.");
         }
 
         return mainCategories.stream()
@@ -32,7 +32,7 @@ public class CategoryService {
         final List<Category> subCategories = categoryRepository.findSubAllByMainCategoryId(mainId);
 
         if (subCategories.isEmpty()) {
-            throw new CategoryNotFoundException("지정한 메인 카테고리에 해당하는 서브 카테고리가 없습니다.");
+            throw new UninitializedCategoryException("지정한 메인 카테고리에 해당하는 서브 카테고리가 없습니다.");
         }
 
         return subCategories.stream()
