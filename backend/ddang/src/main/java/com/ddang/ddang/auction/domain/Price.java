6d49774089da36aa2bb0c90595032591823a69fc
apptie <appti@naver.com>
2024-02-14 22:21:55 +0900

    refactor: 예외 처리 로직 변경 및 예외 메세지 변경

--- backend/ddang/src/main/java/com/ddang/ddang/auction/domain/Price.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/domain/Price.java
@@ -18,14 +18,13 @@ public class Price {
     private static final int MINIMUM_PRICE = 0;
     private static final int MAXIMUM_PRICE = 2_100_000_000;
 
+    private static final String EXCEPTION_MESSAGE = "가격은 %d원 이상, %d원 이하여야 합니다.";
+
     private int value;
 
     public Price(final int value) {
-        if (value < MINIMUM_PRICE) {
-            throw new InvalidPriceValueException(String.format("가격은 %d원 이상이어야 합니다.", MINIMUM_PRICE));
-        }
-        if (value > MAXIMUM_PRICE) {
-            throw new InvalidPriceValueException(String.format("가격은 %d원 이하여야 합니다.", MAXIMUM_PRICE));
+        if (value < MINIMUM_PRICE || value > MAXIMUM_PRICE) {
+            throw new InvalidPriceValueException(String.format(EXCEPTION_MESSAGE, MINIMUM_PRICE, MAXIMUM_PRICE));
         }
 
         this.value = value;
